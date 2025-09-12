328745edc9cb9995177b2c17e5e185b27bd94d2b
Anup930 <Anup.gretex@gmail.com>
2025-09-12 13:47:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -125,6 +125,7 @@ A. always Give CGSTAmount, SGSTAmount, IGSTAmount, Don't Inclode in Once
 B. Always Keep Col name Vendor, always keep Col name Company, always keep col name CapitalOrRevenueExpense
 C. Add a Col Part Payment and in val add 0 Every Time,
 D. Memo No, Doc No both are same as invoice No, always Keep In Invoice No
+E.Also We need to show Few Col (CapitalOrRevenueExpense,ExpenseName, AssetGroup, TDSApplicable,TDSRate, TDSAmount, TDSSection, RCMApplicable, GSTInputIncluded, GSTCorrect, CGSTAmount, SGSTAmount, IGSTAmount, FinalAmountPayable, PartPayment, Remarks)
 Return ONLY valid JSON without any extra text, explanation, or markdown formatting.`;
 
 pdfUpload.addEventListener('change', async (event) => {
@@ -472,4 +473,5 @@ hodApprovalBtn.addEventListener('click', openHodApprovalTab);
 finalApprovalBtn.addEventListener('click', openFinalApprovalTab);
 
 // --- END: NEW EVENT LISTENERS ---
+
 
